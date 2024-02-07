48ed34cc3fcc46ebe47562ffd100755c3a0b29e1
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-02-07 06:29:45 -0800

    I complete requests and their management from ui and now we can make post successfully with a ui form!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,28 @@
+import { useRef } from "react";
+import axios from "axios";
+
 const Home = () => {
+  const titleRef = useRef();
+  const viewNumRef = useRef();
+
+  const createPost = () => {
+    const newpost = {
+      title: titleRef.current.value,
+      viewNum: 25,
+    };
+
+    axios
+      .post("http://localhost:27017/api/new-post", newpost)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="bg-red-500 h-[100vh] font-bold text-2xl flex justify-center items-center">
-      Home
+    <div className="bg-red-500 h-[100vh] font-bold text-2xl flex justify-center items-center flex-col gap-4">
+      <input ref={titleRef} type="text" />
+
+      <input ref={viewNumRef} type="number" />
+      <button onClick={() => createPost()}>create</button>
     </div>
   );
 };
